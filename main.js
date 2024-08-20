60fecaef62eb257125a3fafeca765e9673974401
MrMunib <proficientremote@gmail.com>
2024-08-20 22:27:03 +0500

    New Commit

--- main.js
+++ main.js
@@ -2,13 +2,17 @@ function appendValue(element) {
 
   let inputField = document.querySelector(".result");
   
-  if (element.textContent == "AC") {
-    inputField.value = "";
-  } else if (element.textContent == "DEL") {
-    inputField.value = inputField.value.slice(0, -1);
-  } else if (element.textContent == "=") {
-    inputField.value = eval(inputField.value);
-  } else {
-    inputField.value += element.textContent;
+    if (element.textContent == "AC") {
+      inputField.value = "";
+    } else if (element.textContent == "DEL") {
+      inputField.value = inputField.value.slice(0, -1);
+    } else if (element.textContent == "=") {
+      try {
+        inputField.value = eval(inputField.value);
+      } catch (error) {
+        inputField.value = "ERROR";
+      }
+    } else {
+      inputField.value += element.textContent;
+    }
   }
-}
